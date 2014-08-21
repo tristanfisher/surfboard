bfe0907d956bbcfebe116361badd91daaf5e6f4d
Tristan Fisher <code@tristanfisher.com>
2014-08-21 19:53:16 -0400

    caching implemented

--- api/blueprints/weather.py
+++ api/blueprints/weather.py
@@ -1,8 +1,10 @@
 from . import api, api_keys
+from . import Cache
 from flask import jsonify
 from flask import current_app
 import requests
 import json
+from time import time
 
 # If this starts failing, yahoo has a public API as well.
 WUNDERGROUND_URL_BASE="{base}{api_key}{suffix}".format(
@@ -16,50 +18,60 @@ WUNDERGROUND_URL_BASE="{base}{api_key}{suffix}".format(
 @api.route('/weather/<postal_code>', methods=['GET'])
 def return_weather_postal_code(postal_code):
 
-    #check cache for postal_code first.
-    #https://api.wunderground.com/api/0002f35bcb37c341/forecast/conditions/q/11201.json
-    WUNDERGROUND_URL = WUNDERGROUND_URL_BASE+postal_code+".json"
+    cache = Cache('weather_cache')
+    cache_result = cache.load()
 
-    r = requests.get(WUNDERGROUND_URL)
-
-    if r.status_code == 200:
-        response = r.text
-        #if 'application/json' not in r.headers['content-type']:
-        try:
-            response = json.loads(response)
-        except ValueError:
-            return jsonify(status="Received invalid response from API (Could not parse JSON)"), 500
-
-        weather_data = {}
-
-        weather_data['data_source'] = 'wunderground'
-
-        co = response['current_observation']
-        weather_data['city'] = co['display_location']['city']
-        weather_data['state'] = co['display_location']['state']
-        weather_data['country'] = co['display_location']['country']
-        weather_data['zip'] = co['display_location']['zip']
-        weather_data['latitude'] = co['display_location']['latitude']
-        weather_data['longitude'] = co['display_location']['longitude']
-        weather_data['observation_epoch'] = co['observation_epoch']
-        weather_data['weather'] = co['weather']
-        weather_data['temp_f'] = co['temp_f']
-        weather_data['temp_c'] = co['temp_c']
-        weather_data['wind_string'] = co['wind_string']
-
-        sforecastday = response['forecast']['simpleforecast']['forecastday']
-        weather_data['forecast'] = {}
-
-        for day in sforecastday:
-            weather_data['forecast'][day['date']['day']] = {
-                'conditions': day['conditions'],
-                'day': day['date']['day'],
-                'high': day['low'],
-                'low': day['high']
-            }  # weather_data['forecast'][22]
-
-        return json.dumps(weather_data)
-
+    if cache_result:
+        return json.dumps(cache_result)
     else:
-        return jsonify(response='There was an error fetching the weather.', status_code=r.status_code)
+
+        #check cache for postal_code first.
+        #https://api.wunderground.com/api/0002f35bcb37c341/forecast/conditions/q/11201.json
+        WUNDERGROUND_URL = WUNDERGROUND_URL_BASE+postal_code+".json"
+
+        r = requests.get(WUNDERGROUND_URL)
+
+        if r.status_code == 200:
+            response = r.text
+            #if 'application/json' not in r.headers['content-type']:
+            try:
+                response = json.loads(response)
+            except ValueError:
+                return jsonify(status="Received invalid response from API (Could not parse JSON)"), 500
+
+            weather_data = {}
+
+            weather_data['data_source'] = 'wunderground'
+
+            co = response['current_observation']
+            weather_data['city'] = co['display_location']['city']
+            weather_data['state'] = co['display_location']['state']
+            weather_data['country'] = co['display_location']['country']
+            weather_data['zip'] = co['display_location']['zip']
+            weather_data['latitude'] = co['display_location']['latitude']
+            weather_data['longitude'] = co['display_location']['longitude']
+            weather_data['observation_epoch'] = co['observation_epoch']
+            weather_data['weather'] = co['weather']
+            weather_data['temp_f'] = co['temp_f']
+            weather_data['temp_c'] = co['temp_c']
+            weather_data['wind_string'] = co['wind_string']
+
+            sforecastday = response['forecast']['simpleforecast']['forecastday']
+            weather_data['forecast'] = {}
+
+            for day in sforecastday:
+                weather_data['forecast'][day['date']['day']] = {
+                    'conditions': day['conditions'],
+                    'day': day['date']['day'],
+                    'high': day['low'],
+                    'low': day['high']
+                }  # weather_data['forecast'][22]
+            weather_data['cache_timestamp'] = int(time())
+
+            cache.save(weather_data)
+            weather_data = json.dumps(weather_data)
+            return weather_data
+
+        else:
+            return jsonify(response='There was an error fetching the weather.', status_code=r.status_code)
 
