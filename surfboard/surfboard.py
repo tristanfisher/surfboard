98faa2497b766c18bdd312224281d8df168ac98f
Tristan Fisher <code@tristanfisher.com>
2014-08-26 16:11:46 -0400

    send api keys from server

--- surfboard/surfboard.py
+++ surfboard/surfboard.py
@@ -2,8 +2,19 @@ from flask import Flask, render_template, redirect, json, url_for
 from flask import session, request
 from flask import flash
 from flask import abort
-
 import os
+
+def get_config_from_json(json_file='./api_keys.json'):
+    try:
+        with open(json_file) as key_file:
+            keys = json.load(key_file)
+            return keys
+    except FileNotFoundError as inst:
+        import sys
+        err, msg = inst.args
+        print("Couldn't load API key file.  Error: " + msg, file=sys.stderr)
+
+api_keys = get_config_from_json()
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('FLASK_CONFIG') or 'config')
@@ -18,7 +29,7 @@ if platform.system() == 'Darwin':
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', cells=6), 200
+    return render_template('index.html', api_keys=api_keys, cells=6), 200
 
 
 if __name__ == '__main__':
